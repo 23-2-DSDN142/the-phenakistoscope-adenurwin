454739b2cbd00aaf3709a5423e7399b4b8e6d01f
adenurwin <adenurwin@gmail.com>
2023-09-14 23:18:40 +1200

    final

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,22 +1,18 @@
-const SLICE_COUNT = 10;
+const SLICE_COUNT = 14;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_PRINT(A3));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("crab" , "png");
-
 }
 
 function setup_layers(pScope){
-
-  new PLayer(null, "#C2B280");  //lets us draw the whole circle background, ignoring the boundaries
-
+  new PLayer(null, "#D9BEA1");  //lets us draw the whole circle background, ignoring the boundaries
   var layer1 = new PLayer(crab);
-  layer1.mode( SWIRL(3) );
-  layer1.set_boundary( 200, 1000 );
+  layer1.mode(SWIRL(2));
+  layer1.set_boundary(200,1000);
 
   var layer2 = new PLayer(bird);
   layer2.mode( RING );
@@ -28,49 +24,42 @@ function setup_layers(pScope){
 }
 
 function crab(x, y, animation, pScope){
-  
   scale(animation.frame*2); // makes bigger over time
-
-  scale(0.1);
+  scale(.1);
   rotate(-90);
-
+  fill("red")
   let crabY = animation.wave(2)*500;
-  pScope.draw_image("crab",x,crabY);
-
-
+  rect(x,crabY,1000,800,400);
+  arc(x+1000,crabY,500,400,10,260);
+  arc(x,crabY,500,400,280,170);
+  rect(x+300,crabY-200,100,200,400);
+  rect(x+600,crabY-200,100,200,400);
 }
 
 function bird(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
+  let angleOffset = (360/SLICE_COUNT)/2;
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
-
-  fill(34,139,34)
+  fill("#50C878");
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
   fill(255)
-
-  rect(-20,-350,70,100,100) // .wave is a cosine wave btw
-
-  rect(-10,-350-animation.wave()*50,50,60,100) // .wave is a cosine wave btw
-
+  rect(-20,-350,70,100,100);
+  rect(-10,-350-animation.wave()*50,50,60,100);
+  fill("orange");
+  rect(x+5,-360-animation.wave()*50,20,30,100);
 }
 
 function water(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-
-  fill(255)
-  rect(-300,-900-animation.wave()*100,800,20) // .wave is a cosine wave btw
-
-
-  fill(14,135,204)
-  rect(-400,-1100-animation.wave()*100,800,200) // .wave is a cosine wave btw
-
+  noStroke();
+  // wave back
+  fill("#255E65");
+  rect(-400,-1000-animation.wave()*100,800,200,200);
+  // wave mid
+  fill("#3591A0");
+  rect(-300,-850-animation.wave()*150,800,150,200,200,0,0);
+  // wave front
+  fill(255);
+  rect(-300,-700-animation.wave()*150,800,20);
 }
